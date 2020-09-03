6894e93a3f0513f47b7292b235759382c9e63e0f
Michał Gilewski <michal.gilewski@start-up.house>
2020-09-03 22:17:44 +0200

    Firstly use explicit bucket settings, secondly use paperclip defaults

--- lib/paperclip/storage/backblaze.rb
+++ lib/paperclip/storage/backblaze.rb
@@ -81,7 +81,7 @@ module Paperclip
       # Return the Backblaze::B2::Bucket object representing the bucket
       # specified by the required options[:b2_bucket].
       def b2_bucket
-        bucket_name = @options[:bucket] || b2_credentials.fetch(:bucket)
+        bucket_name = b2_credentials.fetch(:bucket) || @options[:bucket]
         @b2_buckets[bucket_name] ||= ::Backblaze::B2::Bucket.get_bucket(
           name: bucket_name
         )
